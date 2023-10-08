4c06edcbb0ea2081e3512bb5a6318d8faea97c85
Fakhry <fakhry.linux@gmail.com>
2023-10-08 12:22:48 +0800

    Query Builder Delete

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -183,5 +183,15 @@ class QueryBuilderTest extends TestCase
         });
     }
 
+    public function testDelete()
+    {
+        $this->insertCategories();
+
+        DB::table("categories")->where("id", '=', "SMARTPHONE")->delete();
+
+        $collection = DB::table("categories")->where("id", "=", "SMARTPHONE")->get();
+        self::assertCount(0, $collection);
+    }
+
 
 }
