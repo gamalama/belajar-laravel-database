b7cae77c264d790564122cf226aa88c4a8c67f50
Fakhry <fakhry.linux@gmail.com>
2023-10-08 11:24:57 +0800

    Query Builder Select

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -5,7 +5,9 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
+use function Laravel\Prompts\select;
 
 class QueryBuilderTest extends TestCase
 {
@@ -30,4 +32,19 @@ class QueryBuilderTest extends TestCase
         $result = DB::select("select count(id) as total from categories");
         self::assertEquals(2, $result[0]->total);
     }
+
+    public function testSelect()
+    {
+        $this->testInsert();
+
+        $collection = DB::table("categories")->select(["id", "name"])->get();
+        self::assertNotNull($collection);
+
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+
+    }
+
+
 }
